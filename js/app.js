b7a1bd35f23e33d65fdb7dce9579e0aedf9422dd
mmantle-hud <m.e.mantle@hud.ac.uk>
2022-11-15 08:47:13 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-const dt = luxon.DateTime;
-const london = dt.now().setZone('Europe/London');
-const ny = dt.now().setZone('America/New_York');
+const DateTime = luxon.DateTime;
+const london = DateTime.now().setZone('Europe/London');
+const ny = DateTime.now().setZone('America/New_York');
 console.log(`Time in London is ${london.hour}:${london.minute}`);
 console.log(`Time in New York is  ${ny.hour}:${ny.minute}`);
